5fbd4ec430b830b2d2c2228e7b915f98f715603e
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-04-29 12:22:52 +0600

    feat(zsh): added better plugins manager

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,19 +1,19 @@
 #!/bin/env zsh
 
 # >>> plugins integration >>>
-__scripts=(
-	"/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-	"/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-	"/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-	"/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-	"/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-	"/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-	"/usr/share/fzf/shell/key-bindings.zsh"
-	"/usr/share/fzf/key-bindings.zsh"
+__plugins=(
+	"/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh:/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh:/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+	"/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh:/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh:/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+	"/usr/share/fzf/shell/key-bindings.zsh:/usr/share/fzf/key-bindings.zsh"
 	"/usr/share/fzf/completion.zsh"
 )
 
-for __script in $__scripts; do
-	{ [ -f "$__script" ] && . "$__script" ;} || true
+for __plugin in $__plugins; do
+	__scripts=(${(@s/:/)__plugin})
+	for __script in ${__scripts[@]}; do
+		{ [ -f "$__script" ] && . "$__script";} || true
+	done
 done
+
+unset __plugins __plugin __scripts __script
 # <<< plugins integration <<<
