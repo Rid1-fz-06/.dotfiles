72825f0c89ddbe4815cf98a60431e0858a492ba4
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-04-29 11:16:45 +0600

    feat(zsh): changed color of prompt sign

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -13,6 +13,6 @@ function precmd() {
 }
 
 # PS1='%B%F{blue}[%F{red}%n%F{white}@%F{green}%m %F{yellow}%1~%F{blue}]%f%b %(?.%F{blue}󰁕%f.%F{red}󰁕%f) '
-PS1='%F{magenta}%f%F{yellow}%f%F{green}%f%b '
+PS1='%F{magenta}%f%F{yellow}%f%F{blue}%f%b '
 RPS1='%B%F{magenta}$vcs_info_msg_0_%f%b%B%F{blue}%(1j. [ %j   ].)%f%b%(?..%F{red} %f)'
 # <<< prompt <<<
