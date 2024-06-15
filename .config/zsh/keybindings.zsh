da5d54845a89761757c49133260dcf5243863db0
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-06-15 10:05:33 +0600

    feat(zsh): changed keyboard shortcut mode to vim mode

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -1,5 +1,15 @@
 #!/bin/env zsh
 
 # >>> zsh keybindings >>>
-bindkey -e
+bindkey -v
+
+function vi-yank-wl-copy {
+    zle vi-yank
+   echo "$CUTBUFFER" | wl-copy
+}
+zle -N vi-yank-wl-copy
+
+bindkey -M vicmd 'y' vi-yank-wl-copy
+bindkey "^H" backward-delete-char
+bindkey "^?" backward-delete-char
 # <<< zsh keybindings <<<
