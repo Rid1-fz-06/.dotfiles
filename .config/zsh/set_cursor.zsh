af39d47e40de1e1e064e01d7463d4ce940940588
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-06-25 19:52:33 +0600

    feat(zsh): changed keybindings to emacs style

--- .config/zsh/set_cursor.zsh
+++ .config/zsh/set_cursor.zsh
@@ -1,19 +1,24 @@
+#!/bin/env zsh
+
+# >>> cursor >>>
 export KEYTIMEOUT=1
 
-function zle-keymap-select () {
-  case $KEYMAP in
-  vicmd ) echo -ne '\e[2 q';; # block
-  viins | main ) echo -ne '\e[4 q';; # underline
-  esac
-
-}
-zle -N zle-keymap-select
-
-zle-line-init() {
-  zle -K viins
-  echo -ne "\e[4 q"
-}
-zle -N zle-line-init
+# function zle-keymap-select () {
+#   case $KEYMAP in
+#   vicmd ) echo -ne '\e[2 q';; # block
+#   viins | main ) echo -ne '\e[4 q';; # underline
+#   esac
+#
+# }
+# zle -N zle-keymap-select
+#
+# zle-line-init() {
+#   zle -K viins
+#   echo -ne "\e[4 q"
+# }
+# zle -N zle-line-init
 
 echo -ne '\e[4 q'
 preexec() { echo -ne '\e[4 q' ;}
+
+# <<< cursor <<<
